51ce7fabae19c00e70567f2453d7a97f6960f0d5
Crystal Word <cword1991@gmail.com>
2022-02-05 02:33:25 -0500

    adding basic pagination functionality

--- src/Components/characters.js
+++ src/Components/characters.js
@@ -1,32 +1,64 @@
-import React, {  useState , useEffect} from 'react';
+import React, {  useState , useEffect } from 'react';
 import axios from 'axios';
-
 
 
 const Characters = () => {
 
     const [characters, setCharacters] = useState([]);
-
-
+    const [page, setPage] = useState(1);
+    
     useEffect(() => {
-        
-        axios.get('https://swapi.dev/api/people/').then(res => {
+      
+            axios.get(`http://swapi.dev/api/people/?page=${page}`).then(res => {
+                
+               setCharacters(res.data.results);
+            })
             
-           setCharacters(res.data.results);
-        })
+            // .catch((err) => {
+            //     console.log(err)
+            // })
+    
+            // .finally(() => {
+            //     console.log("Operation Completed")
+               
+            // })
+        console.log(page)
         
-        // .catch((err) => {
-        //     console.log(err)
-        // })
+    }, [page, setCharacters]);
 
-        // .finally(() => {
-        //     console.log("Operation Completed")
-           
-        // })
+    const nextPageClick = () => {
+        if (page <= 8) {
+            
+            setPage(page + 1)
 
-    }, []);
+          
+    
+        }
+        
+    }
+
+    const previousPageClick = () => {
+    if (page >= 2) {
+        
+        setPage(page - 1)
+
+    }
+
+    
+}
 
 
+function setPageNumber(value) {
+    
+    setPage(Number(value.target.innerText))
+    // value.target.classList.add('active')
+    console.log(value.classList)
+    
+ 
+ }
+
+
+   
 
     return ( <div> {
     !characters ? ("Character Not Found"): (
@@ -60,12 +92,35 @@ const Characters = () => {
     )}
     <nav className='d-flex justify-content-center'>
         <ul className='pagination'>
-            <li className='page-link'>Previous Page</li>
-            <li className='page-link'>Next Page</li>
+        {page === 1 ? "" :
+            <li className='page-link' onClick={previousPageClick}>Previous Page</li>   
+            
+        }
+
+            <li className='page-item page-link' onClick={setPageNumber}>1</li>
+            <li className='page-link' onClick={setPageNumber}>2</li>
+            <li className='page-link' onClick={setPageNumber}>3</li>
+            <li className='page-link' onClick={setPageNumber}>4</li>
+            <li className='page-link' onClick={setPageNumber}>5</li>
+            <li className='page-link' onClick={setPageNumber}>6</li>
+            <li className='page-link' onClick={setPageNumber}>7</li>
+            <li className='page-link' onClick={setPageNumber}>8</li>
+            <li className='page-link' onClick={setPageNumber}>9</li>
+            {/* <li  className="page-item active" onClick={setPageNumber}>
+                <span className='page-link page-item active'>9</span>
+            </li> */}
+
+            {page === 9 ? "" :
+            <li className='page-link text-red'onClick={nextPageClick}>Next Page</li>
+            }
+           
         </ul>
     </nav>
     </div> 
     );
 };
- 
+
+
 export default Characters;
+
+
